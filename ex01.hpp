ec67f9e2bf006127149f3ea87cf8b111b06fcacf
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2022-03-04 15:48:25 +0300

    updated

--- ex01.hpp
+++ ex01.hpp
@@ -1,8 +1,7 @@
-
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 void multiples (int& sum, int x, int n)
 {
-
+  sum = (n * (n + 1) >> 2) * x;
 }
